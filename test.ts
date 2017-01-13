53eabee23431a6014f962e2e5928ad21bcd98a01
Ilya Krasnovsky <IKrasnovsky@corp.idtus.com>
2017-01-13 18:13:33 -0500

    basic working form

--- test.ts
+++ test.ts
@@ -4,7 +4,8 @@ class Form_Data
     private last_name : string;
     public constructor(first_name : string, last_name : string)
     {
-
+        this.first_name = first_name;
+        this.last_name = last_name;
     }
     public get_first_name() : string
     {
@@ -15,3 +16,20 @@ class Form_Data
         return this.last_name;
     }
 }
+
+function get_form_data() : Form_Data
+{
+    let first_name = document.forms.namedItem("form1")["form1_firstname"].value;
+    let last_name = document.forms.namedItem("form1")["form1_lastname"].value;
+    let form_data = new Form_Data(first_name, last_name);
+    console.log(form_data.get_first_name() + " " + form_data.get_last_name());
+    return form_data;
+}
+/*
+function main() : void
+{
+
+}
+
+document.addEventListener('DOMContentLoaded', main);
+*/
